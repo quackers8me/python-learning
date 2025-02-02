3152537306fcdf8b46ba3e3ada4de1bf7de943aa
Jason Ramsey <quackers8me@users.noreply.github.com>
2025-02-02 08:23:15 -0800

    Add joker
    F for fool = joker

--- cards2unicode.py
+++ cards2unicode.py
@@ -3,8 +3,8 @@ import sys
 if sys.version_info.major == 3:
     unichr = chr
 
-FACES = 'BA23456789TJCQK'
-UNICODE_FACES = '0123456789ABCDE'
+FACES = 'FBA23456789TJCQK'
+UNICODE_FACES = 'F0123456789ABCDE'
 SUITS = 'SHDC'
 UNICODE_SUITS = 'ABCD'
 COLORS = ['\x1b[%dm' % c for c in (30, 31, 34, 32, 39)]
@@ -15,6 +15,8 @@ def unicard(card, color=False):
         card = 'T' + card[2]
     if card[:1].upper() == 'B':
         face, suit = 'BS'
+    if card[:1].upper() == 'F':
+        face, suit = 'FC'
     else:
         face, suit = card.upper()
     c = unichr(int('0001f0%s%s' % (
